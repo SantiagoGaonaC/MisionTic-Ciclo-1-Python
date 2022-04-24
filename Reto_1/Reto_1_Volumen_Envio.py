d9459b093b3b43b40fa5115940a000b81c254f39
SantiagoGaonaC <sgaonacarvajal@outlook.com>
2022-04-24 17:33:04 -0500

    Reto_1

---
+++ Reto_1/Reto_1_Volumen_Envio.py
@@ -0,0 +1,12 @@
+alto = float(input())
+ancho = float(input())
+profundo = float(input())
+volumen = float(alto*ancho*profundo)
+costo = volumen*12
+
+print(alto)
+print(ancho)
+print(profundo)
+print(volumen)
+print(costo)
+#SIN DIFICULTAD ALGUNA 0/10
